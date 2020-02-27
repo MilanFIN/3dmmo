49b59b57855dfe9a9a96cd1f3c30747d0d39820a
milan <milan@Python.python>
2020-02-27 15:51:54 +0200

    login info to client

--- test1/scripts/gamecontroller.gd
+++ test1/scripts/gamecontroller.gd
@@ -13,8 +13,8 @@ func _ready():
 	ws.connect("connection_established", self, "_connection_established")
 	ws.connect("connection_closed", self, "_connection_closed")
 	ws.connect("connection_error", self, "_connection_error")
-	#ws.connect    data_received ( )
 
+	ws.connect("data_received", self, "_data_received")
 	var url = "ws://"+address+":"+port
 	print("Connecting to " + url)
 	ws.connect_to_url(url)
@@ -35,7 +35,8 @@ func _ready():
 
 func _connection_established(protocol):
 	print("Connection established with protocol: ", protocol)
-
+	var d = {"action":"login","username":"1", "password":"1"}
+	ws.get_peer(1).put_packet(JSON.print(d).to_ascii())
 func _connection_closed():
 	print("Connection closed")
 
@@ -47,7 +48,13 @@ func _physics_process(delta):
 	if (ws):
 		pass
 
+func _data_received(p_id = 1):
+	var packet = ws.get_peer(1).get_packet().get_string_from_utf8()
+	#print(str(peer_id))
+	print("got message")
+	print(parse_json(packet))
 
+	
 func _on_timer_timeout():
 	
 	timer.start()
