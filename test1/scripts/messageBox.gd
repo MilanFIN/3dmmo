f4d7725fdd53b6d943236ba9d574fdf69904a762
milan <milan@Python.python>
2020-03-11 15:32:51 +0200

    messaging works

--- test1/scripts/messageBox.gd
+++ test1/scripts/messageBox.gd
@@ -1,17 +1,28 @@
-extends RichTextLabel
+extends Label
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+var messages = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func addMessageBatch(listOfMessages): #{u:name, m:message}
-	print(listOfMessages)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func addMessageBatch(messageList): #{u:name, m:message}
+	for i in messageList:
+		if ("u" in i and "m" in i):
+			messages.append(i["u"] + ": " + i["m"])
+	
+	if (len(messages) > 8):
+		messages = messages.slice(len(messages)-9, len(messages)-1)
+
+
+	text = ""
+	for i in messages:
+		text += i + "\n"
+# Called every frame. 'delta' is the elapsed time since the previous frame.:
 #func _process(delta):
 #	pass
